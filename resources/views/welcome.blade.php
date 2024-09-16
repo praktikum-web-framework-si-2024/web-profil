f5e94d5a49e17212bf30806a20d4d08492d5ae84
novilfahlevy <novilfreon@gmail.com>
2024-09-16 11:56:18 +0800

    pertemuan 1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		
+		<title>Praktikum PWF</title>
+
+		<link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.min.css') }}">
+
+		@vite(['resources/css/app.css', 'resources/js/app.js'])
+	</head>
+	<body>
+
+		<div class="d-flex flex-column align-items-center">
+			<img src="{{ asset('assets/images/gunpark.jpeg') }}" class="img-thumbnail rounded-circle d-inline-block mb-3" width="200" height="200" alt="Gun Park">
+			<h1 class="d-inline-block mb-3">Muhammad Novil Fahlevy</h1>
+			<p>NIM. 2109116095</p>
+		</div>
+		
+		<script src="{{ asset('assets/bootstrap/bootstrap.min.js') }}"></script>
+	</body>
+</html>
